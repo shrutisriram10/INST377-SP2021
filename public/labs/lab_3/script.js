b0fc6fc457c7b6c48faaefc4c38f5b03143d614c
shrutisriram10 <78097685+shrutisriram10@users.noreply.github.com>
2021-02-12 14:10:51 -0500

    lab 3: background and title

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,16 +1,22 @@
-    let i = 1;
+      
+      let i = 1;
       for(let li of carousel.querySelectorAll('li')) {
         li.style.position = 'relative';
         li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
         i++;
       }
       /*
-      const array1 = querySelectorAll
+      const array1 = [];
+      const listContainer = document.createElement('ul')
+      const target = document.querySelector('#box1')
+      target.append(listContainer);
+
       array1.forEach(element => {
         element.style.position = 'relative';
         element.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
         i++;
       });*/
+
       let width = 130;
       let count = 3; 
   
